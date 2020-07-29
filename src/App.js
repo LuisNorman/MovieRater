134e51f29b578c563332bd30c049c06f39feb277
LuisNorman <lanorman14@gmail.com>
2020-07-29 13:40:36 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [movies, setMovie] = useState([]);
+
+  useEffect(()=>{
+    fetch("http://127.0.0.1:8000/api/movies/", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token 81e7524cbfe55a22b7133d15580b3b6baf3af7f2',
+      }
+    }).then(resp => resp.json()) // fetch the response then convert to json
+    .then(resp => setMovie(resp))
+    .catch(error => console.log(error))
+  }, []) // array at end means only run effect once
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Movie Rater</h1>
       </header>
+      <div className="layout"> 
+          <div>
+            { movies.map(movie => {
+              return <h2>{movie.title}</h2>
+            } ) }
+          </div>
+          <div>Movie Details</div>
+        </div>
     </div>
   );
 }
